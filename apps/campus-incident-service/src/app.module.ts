58ff1220929b02e01a0dfa5c102d1a5a7f268ab2
Diego050109 <lemadiego597@gmail.com>
2026-06-02 14:56:58 -0500

    fix: use environment variables for database connection

--- apps/campus-incident-service/src/app.module.ts
+++ apps/campus-incident-service/src/app.module.ts
@@ -8,11 +8,11 @@ import { Incident } from './domain/entities/incident.entity';
   imports: [
 TypeOrmModule.forRoot({
   type: 'postgres',
-  host: 'localhost',
-  port: 5433,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'campus_incidents_db',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'campus_incidents_db',
   entities: [Incident],
   synchronize: true,
 }),

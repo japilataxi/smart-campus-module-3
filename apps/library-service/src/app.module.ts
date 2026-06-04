5eb7eb000d93c44f8e55d155530792e648a15990
japilataxi <japilataxi@uce.edu.ec>
2026-06-03 22:34:58 -0500

    feat(library-service): implement books authors categories and loans

--- apps/library-service/src/app.module.ts
+++ apps/library-service/src/app.module.ts
@@ -1,12 +1,47 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Author } from './authors/entities/author.entity';
+import { Category } from './categories/entities/category.entity';
+import { Book } from './books/entities/book.entity';
+import { Loan } from './loans/entities/loan.entity';
+
+import { AuthorsModule } from './authors/authors.module';
+import { CategoriesModule } from './categories/categories.module';
+import { BooksModule } from './books/books.module';
+import { LoansModule } from './loans/loans.module';
+
 import { HealthModule } from './health/health.module';
 import { MetricsModule } from './metrics/metrics.module';
 
 @Module({
-  imports: [HealthModule, MetricsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url:
+        process.env.DATABASE_URL ||
+        'postgresql://library_user:library_password@localhost:5434/library_db',
+      entities: [
+        Author,
+        Category,
+        Book,
+        Loan,
+      ],
+      synchronize: true,
+    }),
+
+    AuthorsModule,
+    CategoriesModule,
+    BooksModule,
+    LoansModule,
+
+    HealthModule,
+    MetricsModule,
+  ],
 })
 export class AppModule {}
